371037372044fa7b620b0fdfb66691a71f23f06c
Jan Kaiponen <2504255+itsjan@users.noreply.github.com>
2024-09-13 14:34:29 +0300

    UI: fetch assignments  from the API

--- programming-ui/src/components/Assignment.svelte
+++ programming-ui/src/components/Assignment.svelte
@@ -1,9 +1,12 @@
 <script>
+    import { onMount } from 'svelte';
 
-    const assignments = [
-      {"id":1,"title":"Hello","assignment_order":1,"handout":"Write a function \"hello\" that returns the string \"Hello\"","test_code":"import socket\ndef guard(*args, **kwargs):\n  raise Exception(\"Internet is bad for you :|\")\nsocket.socket = guard\n\nimport unittest\nfrom code import *\n\nclass TestHello(unittest.TestCase):\n\n  def test_hello(self):\n    self.assertEqual(hello(), \"Hello\", \"Function should return \\\"Hello\\\"\")\n"},
-      {"id":2,"title":"Hello world","assignment_order":2,"handout":"Write a function \"hello\" that returns the string \"Hello world!\"","test_code":"import socket\ndef guard(*args, **kwargs):\n  raise Exception(\"Internet is bad for you :|\")\nsocket.socket = guard\n\nimport unittest\nfrom code import *\n\nclass TestHello(unittest.TestCase):\n\n  def test_hello(self):\n    self.assertEqual(hello(), \"Hello world!\", \"Function should return \\\"Hello world!\\\"\")\n"},
-      {"id":3,"title":"Sum","assignment_order":3,"handout":"Write a function \"sum\" that takes two numbers as parameters and returns their sum.","test_code":"import socket\ndef guard(*args, **kwargs):\n  raise Exception(\"Internet is bad for you :|\")\nsocket.socket = guard\n\nimport unittest\nfrom code import *\n\nclass TestHello(unittest.TestCase):\n\n  def test_sum_1(self):\n    self.assertEqual(sum(2, 4), 6, \"Call sum(2, 4) should return 6.\")\n\n  def test_sum_2(self):\n    self.assertEqual(sum(40, 2), 42, \"Call sum(40, 2) should return 42.\")\n"}]
+    let assignments = [];
+
+    onMount(async () => {
+        const response = await fetch('/api/assignments');
+        assignments = await response.json();
+    });
     import AssignmentSelector from "./AssignmentSelector.svelte";
 
 
--- programming-ui/vite.config.js
+++ programming-ui/vite.config.js
@@ -1,10 +1,5 @@
 // vite.config.js
 const config = {
-  //...
-  //optimizeDeps: {
-  //  exclude: ["prism-code-editor/web-component", "prism-code-editor"],
-  //},
-  //...
 };
 
 export default config;
