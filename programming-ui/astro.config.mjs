a60d06b2605854eac71269cebd905b36ea7adc3d
Jan Kaiponen <2504255+itsjan@users.noreply.github.com>
2024-09-19 16:30:15 +0300

    UI: redirect the user to /userUuid

--- programming-ui/astro.config.mjs
+++ programming-ui/astro.config.mjs
@@ -4,8 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [svelte(), tailwind()],
+  output: "server",
   server: {
     port: 3000,
-    host: true
-  }
+    host: true,
+  },
 });
