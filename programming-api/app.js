2f3aef4920f44559c5d806222c75c077905ccbef
Jan Kaiponen <2504255+itsjan@users.noreply.github.com>
2024-09-29 08:54:59 +0300

    clean up programming api app

--- programming-api/app.js
+++ programming-api/app.js
@@ -1,5 +1,4 @@
 // app.js
-//import { Hono } from "https://deno.land/x/hono@v4.3.11/mod.ts";
 import { Hono } from "hono";
 import { upgradeWebSocket } from "hono/deno";
 import * as assignments from "./controllers/programmingAssignmentsController.js";
@@ -12,6 +11,8 @@ const clients = new Map();
 
 app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
 
+// Web Socket for communication with the programming-ui clients
+// Currently only used to send updates to grading requests
 app.get(
   "/ws/user/:userUuid",
   upgradeWebSocket((c) => {
@@ -21,12 +22,8 @@ app.get(
           `WebSocket connection opened for user: ${c.req.param("userUuid")}`,
         );
         clients.set(c.req.param("userUuid"), ws);
-        const data = JSON.stringify({
-          type: "hello",
-          message: "Hello from server!",
-        });
-        ws.send(data);
       },
+      // Messages from the client would be handled here
       onMessage(event, ws) {
         console.log(`Message from client: ${event.data}`);
         ws.send("Hello from server!");
@@ -38,62 +35,40 @@ app.get(
   }),
 );
 
+// Returns all programming assignments
+// Candidate for caching
 app.get("/api/assignments", assignments.getAssignments);
 
+// Endpoint for the programming-ui client to submit solutons for grading
 app.post("/api/user/:userUuid/submissions/:assignmentId", (c) => {
-  console.log("Starting post submissions function");
   const ws = clients.get(c.req.param("userUuid"));
   return submissions.submitSolutionForGrading(c, ws);
 });
 
-app.get(
-  "/api/user/:userUuid/submissions/:assignmentId",
-  submissions.getSubmissionsByUser,
-);
-
+// Returns all submissions for a given user
 app.get("/api/user/:userUuid/submissions", submissions.getAllSubmissionsByUser);
 
+// Polls grading results from the Redis queue
+// Also sends updates to the clients
 async function pollResults() {
   const client = await getRedisClient();
 
   while (true) {
     try {
       // Pop a message from the right end of the 'results' list
-      client.rpop("results").then(async (message) => {
+      client.rpop("results").then((message) => {
         if (message !== null) {
-          console.log("Received result:", message);
           const result = JSON.parse(message);
-          console.log("Parsed result:", result);
 
-          console.log("Updating grader feedback with params:");
-          console.log("user_uuid:", result.user_uuid);
-          console.log("id:", result.id);
-          console.log("grader_feedback:", result.grader_feedback);
-          console.log("correct:", result.correct);
-          console.log("status:", result.status);
-
-          const updatedResult = await updateGraderFeedback(
-            result.user_uuid,
-            result.id,
-            result.grader_feedback,
-            result.correct,
-            result.status,
-          );
-
-          console.log("Updated result:");
-          console.log(JSON.stringify(updatedResult, null, 2));
-
+          // Send update to the client
           const ws = clients.get(result.user_uuid);
           if (ws) {
-            console.log("Sending WebSocket message to user:", result.user_uuid);
             ws.send(
               JSON.stringify({
                 type: "submission_update",
                 submission: result,
               }),
             );
-          } else {
-            console.log("WebSocket not found for user:", result.user_uuid);
           }
         } else {
           //console.log("No message received from Redis queue");
@@ -109,11 +84,7 @@ async function pollResults() {
   }
 }
 
-//const intervalId = setInterval(
-//  () => console.log("Nope"),
-//  5000);
-//clearInterval(intervalId);
-
+// Start the polling loop
 pollResults();
 
 export default app;
