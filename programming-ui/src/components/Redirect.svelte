eebab8ab1f20294cd9cb9258279367dcec3a5f8f
Jan Kaiponen <2504255+itsjan@users.noreply.github.com>
2024-09-30 16:43:23 +0300

    Redirect

---
+++ programming-ui/src/components/Redirect.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { userUuid } from "../stores/stores.js";
+    import { selectedAssignment } from "../stores/assignments.svelte.js";
+    import { get } from "svelte/store";
+    import { onMount } from "svelte";
+    
+    const currentUserUuid = get(userUuid);
+    const currentSelectedAssignment = get(selectedAssignment) || 1;
+
+    onMount(() => {
+        
+        window.location.href = `/${currentUserUuid}/${currentSelectedAssignment}`
+    });
+
+
+
+</script>
+
+<p>
+    Redirecting to assignment {currentSelectedAssignment} for user {currentUserUuid}...
+</p>
